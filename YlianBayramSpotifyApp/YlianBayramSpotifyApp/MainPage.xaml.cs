09b431f798a8d66a79c389f8d119c89bb0782873
Ylian Fatmi <yli.fatmi@gmail.com>
2023-03-16 17:07:09 +0100

    mais page finie (images placeholder)

--- YlianBayramSpotifyApp/YlianBayramSpotifyApp/MainPage.xaml.cs
+++ YlianBayramSpotifyApp/YlianBayramSpotifyApp/MainPage.xaml.cs
@@ -14,5 +14,21 @@ namespace YlianBayramSpotifyApp
         {
             InitializeComponent();
         }
+
+        async void click(object sender, EventArgs args)
+        {
+            var ListeCases = new List<BoxView> { case1, case2, case3, case4, case5, case6, case7, case8, case9, case10, case11, case12, case13, case14, case15, case16, case17, case18, case19, case20 };
+            for (int i = 0; i < ListeCases.Count; i++)
+            {
+                if (ListeCases[i].Color == Color.White)
+                {
+                    ListeCases[i].Color = Color.Black;
+                } 
+                else
+                {
+                    ListeCases[i].Color = Color.White;
+                }
+            }
+        }
     }
 }
