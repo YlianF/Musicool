81fa85ddf2139ca3f33ee5ae6c6ff0a163abfc51
Ylian Fatmi <yli.fatmi@gmail.com>
2023-04-10 12:32:07 +0200

    artiste page

--- YlianBayramSpotifyApp/YlianBayramSpotifyApp/Pages/ArtistPage.xaml.cs
+++ YlianBayramSpotifyApp/YlianBayramSpotifyApp/Pages/ArtistPage.xaml.cs
@@ -17,7 +17,30 @@ namespace YlianBayramSpotifyApp
         public ArtistPage()
         {
             InitializeComponent();
-            this.nomart.Text = SpotifyService.Instance.GetSpotifyClient().Artists.Get("3Rq3YOF9YG9YfCWD4D56RZ").Result.Name;
+
+            // on récupère toutes nos variables avec l'API
+            var imageArtiste = SpotifyService.Instance.GetSpotifyClient().Artists.Get("3Rq3YOF9YG9YfCWD4D56RZ").Result.Images[0].Url;
+            var nomArtiste = SpotifyService.Instance.GetSpotifyClient().Artists.Get("3Rq3YOF9YG9YfCWD4D56RZ").Result.Name;
+            var genresArtiste = SpotifyService.Instance.GetSpotifyClient().Artists.Get("3Rq3YOF9YG9YfCWD4D56RZ").Result.Genres;
+            var nbFollowersArtiste = SpotifyService.Instance.GetSpotifyClient().Artists.Get("3Rq3YOF9YG9YfCWD4D56RZ").Result.Followers.Total;
+            var populariteArtiste = SpotifyService.Instance.GetSpotifyClient().Artists.Get("3Rq3YOF9YG9YfCWD4D56RZ").Result.Popularity.ToString();
+
+
+            // TODO image
+            this.imageArtiste.Source = imageArtiste;
+
+            this.nomArtiste.Text = nomArtiste;
+
+            // on assigne tous les genre de l'artiste à 1 seul label avec une boucle et on met en forme avec le séparateur "|", sauf pour le dernier
+            for (int i = 0;  i <= genresArtiste.Count-2; i++)
+            {
+                this.genresArtiste.Text = this.genresArtiste.Text + genresArtiste[i] + " | ";
+            }
+            this.genresArtiste.Text = this.genresArtiste.Text + genresArtiste[genresArtiste.Count-1];
+
+            this.nbFollowersArtiste.Text = "Nombre de followers : " + nbFollowersArtiste.ToString();
+
+            this.populariteArtiste.Text = "Popularité : " + populariteArtiste + "/100";
         }
     }
 }
--- YlianBayramSpotifyApp/YlianBayramSpotifyApp/Services/SpotifyService.cs
+++ YlianBayramSpotifyApp/YlianBayramSpotifyApp/Services/SpotifyService.cs
@@ -6,7 +6,7 @@ namespace EliseSpotifyApp.Service
 {
     public class SpotifyService
     {
-        const string TokenSpotify = "BQA6TJVjElYoea2aSrQ5_xvXtn_L8rnrvWip58zLdssWyDeIytuOEARfzd6fGdXn3XdtOsr1VxbgnWrxYaZmcMwtisQtPB3mIpDiIhwNacni_wp4YnlLgWbYC-uqNi82UHP5MbHqRGc_sFS_OpNaIp2fZtRVU2niOqBxRmh1_Ziy9T6ENZtlrVLMdl2_8QzfTcXUAj7s_KzOEUow7XUCSAfqaWpABujNtJvVUQ6Qx23nvmnozB4x7hTINrl7oXhyTFqztKJ_u0c95Z8UynmcNDTI8HAXiK5Y1JfZI-rLDmypUZ8rQIZYgyjDs_SosHEbhFscNjR4zm5uTg";
+        const string TokenSpotify = "BQBYzJpOWh0j4MjxIb9uYwj_XSODfwpk_xtuglAQxOobz4odsXtMocSil9QSA9knylnT3xodLXIbiTfzVKURAjs7QiX7U79Qo5k-rjxPnCN1Iqs4QkwZ";
         private SpotifyClient _spotifyClient;
         #region Instance
 
